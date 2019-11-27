994b824eff1616128acd252c34f41bb3ef7bbd18
Andris Reinman <andris@kreata.ee>
2019-11-27 23:35:17 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 cd static
 rm -rf sendmail.tar.gz
-tar czf sendmail.tar.gz ../../forensicat/build/sendmail
+tar czf sendmail.tar.gz ../../forensicat/builds/sendmail
 cd ..
 hugo && git add . && git commit -m update && git push production
 git push
